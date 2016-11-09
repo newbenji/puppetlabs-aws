223d41715cd46ec897555b567e40a892dfef3aa9
Benjamin Andersen <ba@exerp.com>
2016-11-09 15:06:07 +0100

    ..

--- lib/puppet/provider/ec2_vpc_routetable/v2.rb
+++ lib/puppet/provider/ec2_vpc_routetable/v2.rb
@@ -113,9 +113,14 @@ Puppet::Type.type(:ec2_vpc_routetable).provide(:v2, :parent => PuppetX::Puppetla
       
       unless gateway_id
         #nat_gateway_response = ec2.describe_nat_gateways.data.nat_gateways.select { |gateway| gateway.nat_gateway_addresses.first.public_ip == route['gateway'] || gateway.nat_gateway_addresses.first.allocation_id == route['gateway'] }
+        test = ec2.describe_nat_gateways.(filters: [
+                  {name: 'state', values: ['pending', 'available']}
+                  ])
+        Puppet.debug("nat_gateways: #{test}")
         nat_gateway_response = ec2.describe_nat_gateways.(filters: [
           {name: 'state', values: ['pending', 'available']}
           ]).data.nat_gateways.select { |gateway| gateway.nat_gateway_addresses.first.public_ip == route['gateway'] || gateway.nat_gateway_addresses.first.allocation_id == route['gateway'] }
+         
         Puppet.debug("nat_gateways: #{nat_gateway_response}")
         found_nat_gateway = !nat_gateway_response.empty?
       end
